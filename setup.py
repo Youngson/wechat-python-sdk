95474461a6e28afa93c9336de11a75f5ca330b9f
Youngson <Youngson.gu@gmail.com>
2016-04-23 20:48:24 +0800

    Update setup.py
    处理安装是报“UnicodeDecodeError: 'gbk' codec can't decode byte 0xae in position 2: illegal multibyte sequence”的错误。

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 # !/usr/bin/env python
 
 from setuptools import setup, find_packages
+import codecs
 
 setup(
     name='wechat-sdk',
     version='0.6.3',
     keywords=('wechat', 'sdk', 'wechat sdk'),
     description=u'微信公众平台Python开发包',
-    long_description=open("README.rst").read(),
+    long_description=codecs.open("README.rst","r","utf-8").read(),
     license='BSD License',
 
     url='https://github.com/wechat-python-sdk/wechat-python-sdk',
